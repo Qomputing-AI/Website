92fd7b3f1f1e53808fb92ac1c789f511dc9fde3b
kartik16p <kartik16p@gmail.com>
2025-06-21 17:22:18 +0530

    added aboutus constant

--- qomputing-frontend/app/about/page.tsx
+++ qomputing-frontend/app/about/page.tsx
@@ -1,17 +1,13 @@
+import { aboutusContent } from "@/utils/constants/aboutus";
 export default function AboutPage() {
   return (
     <main>
-      <h1>About Us</h1>
+      <h1>{aboutusContent.title}</h1>
       <p>
-        Qomputing is dedicated to advancing the future of technology through quantum computing. 
-        Our goal is to build a powerful, user-friendly Integrated Development Environment (IDE) 
-        specifically designed for quantum developers, researchers, and enthusiasts.
+        {aboutusContent.para1}
       </p>
       <p>
-        With features that support circuit design, real-time simulation, and seamless hardware 
-        integration, Qomputing aims to make quantum computing more accessible and practical. 
-        We believe in simplifying complex concepts and providing the tools needed to innovate, 
-        explore, and build the next generation of computational solutions.
+        {aboutusContent.para2}
       </p>
     </main>
   );
